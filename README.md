934fb70f69ed3251a6771c428eb25ca2d57f82a7
Tim Isonio <timisonio@ucla.edu>
2015-11-20 21:42:50 -0800

    Build offsets now functional. Might be off by one line.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vcf-explorer
+Tim Isonio
+November 2015
+
+A console based tool to view information from a large VCF file.
+
+
+Offset File Format
+------------------
+[VCF file SHA256sum hash]
+1    [byte offset]
+...  [byte offset]
+22   [byte offset]
+X    [byte offset]
+Y    [byte offset]
+MT   [byte offset]
+...
--- build-offsets.py
+++ build-offsets.py
@@ -3,33 +3,76 @@
 import argparse
 import hashlib
 import sys
+import os
+import gzip
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', type=argparse.FileType('r'))
+    parser.add_argument("input")
+    parser.add_argument("--checksum", action="store_true")
     args = parser.parse_args()
 
-    # Compute hash of input file
-    sha = hashlib.sha256()
-    for line in args.input:
-        sha.update(bytes(line, 'utf-8'))
+    # Use file modified time, basically instantaneous
+    input_hash = str(os.stat(args.input).st_mtime) + '\n' + str(os.stat(args.input).st_size)
+    
+    if args.checksum == True:
+        # Compute hash of input file, slow and memory intensive
+        sha = hashlib.sha1()
+        sha.update(open(args.input, 'rb').read())
+        input_hash = input_hash + '\n' + sha.hexdigest()
+    else:
+        input_hash = input_hash + '\n' + '0'
+        
 
-    input_hash = sha.hexdigest()
-    outputname = args.input.name + ".vcfe"
+    outputname = args.input + ".vcfe"
 
     try:
         with open(outputname, 'r') as output:
+            # read in old hash
+            output_hash = []
+            for output_line in output:
+                if output_line.startswith("begin"):
+                    break
+                output_hash.append(output_line.strip())
             # If hash was same as before, no need to continue
-            if output.readline().strip() == input_hash:
-                print("No need to redo offsets; file not changed.")
+            if output_hash == input_hash.split('\n'):
+                print("VCF file not changed. No need to re-create offsets.")
                 sys.exit()
             # File has been changed
             else:
-                print("File changed since last offset creation. Re-creating offsets.")
+                print("VCF file changed since last offset creation. Re-creating offsets.")
     except EnvironmentError:
         # File not present
-        print("File not found, creating offsets.")
+        print("VCFE file not found, creating offsets.")
 
-    with open(args.input.name + '.vcfe', 'w') as output:
-        print(sha.hexdigest(), file=output)
+    with open(outputname, 'w') as output:
+        # Print input file's hash to output file
+        print(input_hash, file=output)
+        print("begin offsets", file=output)
+        
+        with gzip.open(args.input, 'r') as infile:
+            # consume lines until actual content
+            infile_line = infile.readline()
+            while not infile_line.startswith(b"#CHROM"):
+                infile_line = infile.readline()
+
+            offsets = {}
+
+            for infile_line in infile:
+                # vcf format has chromosome as the first column, then a tab
+                infile_line_chromosome = infile_line.split(b'\t')[0]
+                
+                if infile_line_chromosome not in offsets:
+                    # get current position in file
+                    offset = infile.tell()
+                    
+                    # store into dictionary
+                    offsets[infile_line_chromosome] = offset
+                    
+                    # progress update
+                    print("Adding offset", offset, "for chromosome", infile_line_chromosome)
+                    
+                    # print to vcfe file
+                    print(infile_line_chromosome, offset, sep='\t', file=output)
+                    output.flush()
     
